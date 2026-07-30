6e05b1aca84cf6b12668ce04951a3fab0eafab17
Hoaquin42 <jiugny.gg@gmail.com>
2026-07-30 20:32:06 -0300

    Version 1.0.0

--- Public/frontend.js
+++ Public/frontend.js
@@ -1,3 +1,11 @@
+let idEditando = null;
+
+
+
+
+
+
+
 function verGastos(){
 
     const lista = document.getElementById("lista-gastos");
@@ -16,6 +24,31 @@ function verGastos(){
 
             item.textContent = `${gastos.descripcion} - $${gastos.monto} - ${gastos.categoria} - ${gastos.fecha}`
 
+            const botonBorrar = document.createElement("button");
+            botonBorrar.textContent = "Borrar";
+
+            const botonEditar = document.createElement("button");
+            botonEditar.textContent = "Editar";
+
+            botonEditar.addEventListener("click", () =>{
+
+                idEditando = gastos.id;
+
+                document.getElementById("categoria").value = gastos.categoria;
+                document.getElementById("monto").value = gastos.monto;
+                document.getElementById("descripcion").value = gastos.descripcion;
+                document.getElementById("fecha").value = gastos.fecha;
+
+            });
+
+            item.appendChild(botonEditar);
+
+            botonBorrar.addEventListener("click", () => {
+                borrarGasto(gastos.id);
+            });
+
+            item.appendChild(botonBorrar);
+
             lista.appendChild(item);
 
             })
@@ -26,5 +59,74 @@ function verGastos(){
 
 
 }
+
+
+document.getElementById("form-gasto").addEventListener("submit", (evento) => {
+    evento.preventDefault();
+
+    const descripcion = document.getElementById("descripcion").value;
+    const monto = document.getElementById("monto").value;
+    const categoria = document.getElementById("categoria").value;
+    const fecha = document.getElementById("fecha").value;
+
+    if (idEditando === null) {
+
+    fetch("http://localhost:3000/gastos", {
+        
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({ descripcion, monto, categoria, fecha})
+    
+    })
+
+    .then(respuesta => respuesta.json())
+    .then(() => {
+        verGastos();
+        evento.target.reset();
+    })
+
+    .catch(error => console.error("Error", error));
+
+
+} else {
+
+    fetch(`http://localhost:3000/gastos/${idEditando}`, {
+
+        method: "PUT",
+        headers: {"Content-Type": "application/json" },
+        body: JSON.stringify({ descripcion, monto, categoria, fecha})
+    })
+    .then(respuesta => respuesta.json())
+    .then(() => {
+
+        idEditando = null;
+        verGastos();
+        evento.target.reset();
+
+    })
+    .catch(error => console.error("Error:", error));
+
+}
+
+
+});
+
+
+function borrarGasto(id) {
+
+    fetch(`http://localhost:3000/gastos/${id}`, {
+        method: "DELETE"
+    })
+    .then(() => {
+        verGastos();
+    })
+    .catch(error => console.error("Error", error));
+}
+
+
+
 
 verGastos();
